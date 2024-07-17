24357757fb5faf781382f0147c871aef4ea28d64
Xtreila <123803718+Xtreila@users.noreply.github.com>
2024-07-17 11:28:50 +0000

    123

--- script/index.js
+++ script/index.js
@@ -1,16 +1,16 @@
+// popup
 const editButtonElem = document.querySelector('.profile__edid-button'); 
-
-const addButtonElem = document.querySelector('.profile__add-button');
-
 const popupElem = document.querySelector('.popup');
-
-const formElem = document.querySelector('.form');
-
-
 const popupCloseElem = document.querySelector('.popup__close');
 const formElement = document.querySelector('.popup__form');
 
 
+const addButtonElem = document.querySelector('.profile__add-button');
+const formElem = document.querySelector('.form');
+const formCloseElem = document.querySelector('.form__close');
+const formItem = document.querySelector('.form__item');
+const nameImage = document.querySelector('input[name="nameImage"]');
+const galleryName = document.querySelector('.gallery__text');
 
 
 const nameInput = document.querySelector('input[name="nameUser"]');
@@ -18,16 +18,7 @@ const jobInput = document.querySelector('input[name="jobUser"]');
 const profileName = document.querySelector('.profile__name');
 const profileProfession = document.querySelector('.profile__profession');
 
-
-function openForm (element) {
-    element.classList.add('form_opened');
-}
-
-function openForm (element) {
-    element.classList.remove('form_opened');
-}
-
-
+// popup
 function openPopup (element) {
     element.classList.add('popup_opened');
 }
@@ -56,13 +47,75 @@ popupCloseElem.addEventListener('click', () => {
     closePopup(popupElem);
  });
 
+// form
+function openForm (element) {
+    element.classList.add('form_opened');
+}
+
+function closeForm (element) {
+    element.classList.remove('form_opened');
+}
 
 addButtonElem.addEventListener('click', () => {
-    openPopup(formElem);
+    openForm(formElem);
 });
 
+formCloseElem.addEventListener('click', () => {
+    closeForm(formElem);
+ });
+
+
+ function getValueImages () {
+    nameImage.value = galleryName.textContent;
+    // jobInput.value = profileProfession.textContent;
+}
+
+function handleImagesSubmit (evt) {
+    evt.preventDefault(); 
+    galleryName.textContent = nameImage.value;
+    // profileProfession.textContent = jobInput.value;
+    closeForm(formElem);
+   }
+
+   // gallery__image
 
 
 
 
-formElement.addEventListener('submit', handleFormSubmit); 
+
+
+
+
+
+
+formElement.addEventListener('submit', handleFormSubmit);
+formItem.addEventListener('submit', handleImagesSubmit); 
+
+
+
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
